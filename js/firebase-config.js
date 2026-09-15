66b03319f6f3687dbc897c59c234615b89900841
Jonah Nieschwitz <78045916+jjcraze9@users.noreply.github.com>
2026-09-15 18:44:22 -0400

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,9 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyB0jumPEEvj--x_uWvn_Z1zDjs95XVy6KM",
   authDomain: "preschool-pickup.firebaseapp.com",
@@ -14,5 +8,4 @@ const firebaseConfig = {
   appId: "1:409969675462:web:7fed9d70daebef7afe1bc7"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
